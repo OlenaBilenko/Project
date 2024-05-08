f5a3caf7acd3733e8f41771abedb5e744fd3eb88
Olena Bilenko <motuzo4k@gmail.com>
2024-05-08 10:41:21 +0200

    Add log-modal

--- script.js
+++ script.js
@@ -1,100 +1,116 @@
 const bodyWeightChart = document.getElementById("weightChart");
 const bodyTempChart = document.getElementById("tempChart");
 
-new Chart(bodyTempChart, {
-  type: "line",
-  data: {
-    labels: [
-      "2022-01-01",
-      "2022-02-01",
-      "2022-03-01",
-      "2022-04-01",
-      "2022-05-01",
-      "2022-06-01",
-    ], // Changed to date strings
-    datasets: [
-      {
-        //   label: '# of Votes',
-        data: [36.3, 37, 37.3, 39, 38, 37.3],
-        borderColor: "rgb(75, 192, 192)",
-        borderWidth: 1,
-      },
-    ],
-  },
-  options: {
-    scales: {
-      x: {
-        type: "time", // Added to parse dates on the x-axis
-        time: {
-          unit: "hour", // Can be 'day', 'week', 'month', etc.
-          displayFormats: {
-            hour: "HH:mm", // Change this to your preferred display format
+function createGraphs() {
+  new Chart(bodyTempChart, {
+    type: "line",
+    data: {
+      labels: [
+        "2022-01-01",
+        "2022-02-01",
+        "2022-03-01",
+        "2022-04-01",
+        "2022-05-01",
+        "2022-06-01",
+      ], // Changed to date strings
+      datasets: [
+        {
+          //   label: '# of Votes',
+          data: [36.3, 37, 37.3, 39, 38, 37.3],
+          borderColor: "#484282",
+          borderWidth: 1,
+        },
+      ],
+    },
+    options: {
+      scales: {
+        x: {
+          type: "time", // Added to parse dates on the x-axis
+          time: {
+            unit: "hour", // Can be 'day', 'week', 'month', etc.
+            displayFormats: {
+              hour: "HH:mm", // Change this to your preferred display format
+            },
           },
         },
-      },
-      y: {
-        beginAtZero: false,
-      },
-    },
-  },
-});
-
-new Chart(bodyWeightChart, {
-  type: "line",
-  data: {
-    labels: [
-      "2022-01-01",
-      "2022-02-01",
-      "2022-03-01",
-      "2022-04-01",
-      "2022-05-01",
-      "2022-06-01",
-    ], // Changed to date strings
-    datasets: [
-      {
-        //   label: '# of Votes',
-        data: [38.5, 37.1, 37.8, 39.2, 38.8, 37.3],
-        backgroundColor: [
-          "rgba(255, 99, 132, 0.2)",
-          "rgba(255, 159, 64, 0.2)",
-          "rgba(255, 205, 86, 0.2)",
-          "rgba(75, 192, 192, 0.2)",
-          "rgba(54, 162, 235, 0.2)",
-          "rgba(153, 102, 255, 0.2)",
-        ],
-        borderColor: [
-          "rgb(255, 99, 132)",
-          "rgb(255, 159, 64)",
-          "rgb(255, 205, 86)",
-          "rgb(75, 192, 192)",
-          "rgb(54, 162, 235)",
-          "rgb(153, 102, 255)",
-        ],
-        borderWidth: 1,
-      },
-    ],
-  },
-  options: {
-    scales: {
-      x: {
-        type: "time", // Added to parse dates on the x-axis
-        time: {
-          unit: "hour", // Can be 'day', 'week', 'month', etc.
-          displayFormats: {
-            hour: "HH:mm", // Change this to your preferred display format
-          },
+        y: {
+          beginAtZero: false,
         },
       },
-      y: {
-        beginAtZero: false,
+    },
+  });
+
+  new Chart(bodyWeightChart, {
+    type: "line",
+    data: {
+      labels: [
+        "2022-01-01",
+        "2022-02-01",
+        "2022-03-01",
+        "2022-04-01",
+        "2022-05-01",
+        "2022-06-01",
+      ], // Changed to date strings
+      datasets: [
+        {
+          //   label: '# of Votes',
+          data: [38.5, 37.1, 37.8, 39.2, 38.8, 37.3],
+          backgroundColor: "rgb(255, 127, 80)",
+          borderColor: "rgb(255, 127, 80)",
+          borderWidth: 1,
+        },
+      ],
+    },
+    options: {
+      scales: {
+        x: {
+          type: "time", // Added to parse dates on the x-axis
+          time: {
+            unit: "hour", // Can be 'day', 'week', 'month', etc.
+            displayFormats: {
+              hour: "HH:mm", // Change this to your preferred display format
+            },
+          },
+        },
+        y: {
+          beginAtZero: false,
+        },
       },
     },
-  },
-});
-
-async function loadHtml(name) {
-  const response = await fetch(name);
-  const html = await response.text();
-  document.body.innerHTML += html;
+  });
 }
-loadHtml("./log-modal/log-modal.html");
+createGraphs();
+// async function loadHtml(name) {
+//   const response = await fetch(name);
+//   const html = await response.text();
+//   document.getElementById("morecontent").innerHTML += html;
+// }
+
+// async function init() {
+//   await loadHtml("./log-modal/log-modal.html");
+//   createGraphs();
+// }
+
+const btnLog = document.querySelector("body > main > div.add-button > button");
+const dialog = document.querySelector("dialog");
+const dialogClose = document.querySelector("dialog > div > div > button");
+
+btnLog.addEventListener("click", () => {
+  dialog.showModal();
+});
+
+dialogClose.addEventListener("click", () => {
+  dialog.close();
+});
+
+// dialog.addEventListener("click", (e) => {
+//   const dialogDimensions = dialog.getBoundingClientRect();
+//   if (
+//     e.clientX < dialogDimensions.left ||
+//     e.clientX > dialogDimensions.right ||
+//     e.clientY < dialogDimensions.top ||
+//     e.clientY > dialogDimensions.bottom
+//   ) {
+//     dialog.close();
+//   }
+// });
